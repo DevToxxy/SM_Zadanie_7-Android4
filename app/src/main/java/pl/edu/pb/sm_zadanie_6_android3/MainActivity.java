d056eb1504b09669f64a766147a05b304cea7fd1
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2021-11-26 17:40:11 +0100

    Initial commit

---
+++ app/src/main/java/pl/edu/pb/sm_zadanie_6_android3/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.edu.pb.sm_zadanie_6_android3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/pl/edu/pb/sm_zadanie_6_android3/Task.java
@@ -0,0 +1,20 @@
+package pl.edu.pb.sm_zadanie_6_android3;
+import java.util.Date;
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Task {
+    private UUID id;
+    private String name;
+    private Date date;
+    private boolean done;
+
+    public Task(){
+        id = UUID.randomUUID();
+        date = new Date();
+    }
+}
